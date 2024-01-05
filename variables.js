5fb9d51da73f0a8b01eeaaabc8685fcd70867e24
HowardAnti <155449787+HowardAnti@users.noreply.github.com>
2024-01-05 14:05:34 +0000

    variables

---
+++ variables.js
@@ -0,0 +1,51 @@
+var nombre="howard";
+console.log(nombre);
+var apellido="anti";
+console.log(apellido);
+var sueldo=125;
+console.log(sueldo);
+var casado=false;
+console.log(casado);
+var presupuesto=123;
+console.log(presupuesto);
+let gasto=50;
+console.log(gasto);
+const IVA=13;
+console.log(IVA);
+const tipoCambio=6.98;
+console.log(tipo de cambio);
+var uni="umsa";
+console.log(uni);
+if(100%2===0){
+	console.log('100 es par')
+}
+else{
+	console.log('100 no es par')
+}
+
+for(let i=0;i<5;i++){
+	console.log(i);
+}
+
+let contador=10;
+
+while(contador>=0){
+	console.log(contador);
+	contador--;
+
+}
+
+let suma=
+let resta=
+let multi=
+let divi=
+let modulo=
+
+let mayorQue=
+let mayorIgualQue=
+let igualQue=
+let diferenteQue= 
+
+let and=true&&true
+let or=false||true
+let not=!false
