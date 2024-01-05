6b38346657ced51ede1aa3cb6697b3ab6375f789
HowardAnti <155449787+HowardAnti@users.noreply.github.com>
2024-01-05 15:17:30 +0000

    f

--- variab.js
+++ variab.js
@@ -1,1 +1,80 @@
-k
+var nombre="howard";
+console.log(nombre);
+var apellido="anti";
+console.log(apellido);
+var sueldo=125;
+console.log(sueldo);
+var casado=false;
+console.log(casado);
+var presupuesto=123;
+console.log(presupuesto);
+let gasto=50;
+console.log(gasto);
+const IVA=13;
+console.log(IVA);
+const tipoCambio=6.98;
+console.log(tipoCambio);
+var uni="umsa";
+console.log(uni);
+if(100%2===0){
+	console.log('100 es par')
+}
+else{
+	console.log('100 no es par')
+}
+
+for(let i=0;i<5;i++){
+	console.log(i);
+}
+
+let contador=10;
+
+while(contador>=0){
+	console.log(contador);
+	contador--;
+
+}
+
+let suma=5+9
+let resta=8-9
+let multi=6*5
+let divi=8/9
+let modulo=9%5
+
+let mayorQue=8>4
+let mayorIgualQue=8>=2
+let igualQue=5<25
+let diferenteQue=52!=8 
+
+let and=true&&true
+let or=false||true
+let not=!false
+
+const persona={
+	nombre: "pedro",
+	edad: 25,
+	ocupacion: "dasarrollador"
+
+}
+
+const colores=["rojo","verde","azul"];
+
+console.log(persona.edad);
+console.log(persona);
+var h="Mi nombre es"+persona.nombre
+
+persona["apellido"]="Peres";
+console.log(persona);
+
+persona.ocupacion="Programador";
+console.log(ocupacion);
+
+persona["ci"]=16516
+
+console.log(colores);
+console.log(colores.length);
+console.log()
+
+for(let i=0;i<colores.length;i++){
+	console.log("Elemento: "+colres[i]+", indice: "+i);
+}
