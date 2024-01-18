96e55dbd89fe4542994116f93ecdf380e7ba7670
Howard <walterhowardanti@outlook.com>
2024-01-17 22:44:40 -0400

    show

--- practica/show_hide_title/src/components/Title.js
+++ practica/show_hide_title/src/components/Title.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "../styles/Title.css"
+import { useState } from 'react';
+import Switch from '../components/Switch';
 
-function Title({title,show}){
+function Title({title}){
+
+    const[show,setShow]=useState(true);
+
+    const showTitle = ()=>{
+        setShow(!show);
+    }
+
     return(
-        show? <h1 className="capital">{title}</h1> : null
+        <div className="title-container">
+            <Switch text={show? 'Hide':'Show'} onclick={showTitle} />
+            {show? <h1 className="capital">{title}</h1> : null}
+        </div>
     )
 }
 
